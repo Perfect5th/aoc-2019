2b292f2ecdc2f590f2bcda3322cd7c24e45a5450
Mitchell Burton <mitch@mitchellburton.ca>
2020-04-15 15:52:07 -0700

    new day-4-1 and day-4-2 haskell soln

--- day-4/day-4-1.hs
+++ day-4/day-4-1.hs
@@ -1,27 +1,14 @@
-import qualified Data.Char as Char
 
-minval = 264360
-maxval = 746325
+minVal = 264360
+maxVal = 746325
 
-is_ascending :: [Int] -> Bool
-is_ascending []             = True
-is_ascending (x1 : [])      = True
-is_ascending (x1 : x2 : xs) = x1 <= x2 && (is_ascending $ x2 : xs)
+main = do
+    print $ length $ filter validPw [show pw | pw <- [minVal..maxVal]]
 
-has_double :: [Int] -> Bool
-has_double []             = False
-has_double (x1 : [])      = False
-has_double (x1 : x2 : xs) = x1 == x2 || (has_double $ x2 : xs)
+validPw pw = hasDouble pw && increasingOrder pw
 
-expand :: String -> [Int]
-expand str = map Char.digitToInt str
+hasDouble (d1:[]) = False
+hasDouble (d1:d2:ds) = d1 == d2 || hasDouble (d2 : ds)
 
-valid :: Integer -> Bool
-valid pw =
-    let expanded = expand $ show pw
-    in  has_double expanded && is_ascending expanded
-
-solve :: Integer -> Integer -> Int
-solve m0 m1 = length $ filter valid [m0..m1]
-
-main = print $ solve minval maxval
+increasingOrder (d1:[]) = True
+increasingOrder (d1:d2:ds) = d1 <= d2 && increasingOrder (d2 : ds)
--- day-4/day-4-2.hs
+++ day-4/day-4-2.hs
@@ -1,26 +1,14 @@
-import qualified Data.Char as Char
-import qualified Data.List as List
+import Data.List
 
-minval = 264360
-maxval = 746325
+minVal = 264360
+maxVal = 746325
 
-is_ascending :: [Int] -> Bool
-is_ascending []             = True
-is_ascending (x1 : [])      = True
-is_ascending (x1 : x2 : xs) = x1 <= x2 && (is_ascending $ x2 : xs)
+main = do
+    print $ length $ filter validPw [show pw | pw <- [minVal..maxVal]]
 
-has_double :: String -> Bool
-has_double str = any ((== 2) . length) $ List.group str
+validPw pw = hasDouble pw && increasingOrder pw
 
-expand :: String -> [Int]
-expand str = map Char.digitToInt str
+hasDouble pw = 0 < (length $ filter ((==2) . length) $ group pw)
 
-valid :: Integer -> Bool
-valid pw =
-    let expanded = expand $ show pw
-    in  (has_double $ show pw) && is_ascending expanded
-
-solve :: Integer -> Integer -> Int
-solve m0 m1 = length $ filter valid [m0..m1]
-
-main = print $ solve minval maxval
+increasingOrder (d1:[]) = True
+increasingOrder (d1:d2:ds) = d1 <= d2 && increasingOrder (d2 : ds)
