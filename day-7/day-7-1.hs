b5d5400257a50d74baab06b8683550e9a77e48cb
Mitchell Burton <mitch@mitchellburton.ca>
2020-04-17 12:55:01 -0700

    new day-7-1 haskell soln

--- day-7/day-7-1.hs
+++ day-7/day-7-1.hs
@@ -1,141 +1,44 @@
-import qualified Data.List as List
-
-inOp = 3
-outOp = 4
-jiftOp = 5
-jiffOp = 6
-haltOp = 99
-
-data CompState = CompState { position :: Int
-                           , tape :: [Int]
-                           , input :: [Int]
-                           , output :: [Int]
-                           , halted :: Bool
-                           , op :: Int
-                           , vals :: (Int, Int, Int)
-                           , modes :: (Int, Int, Int)
-                           , valP :: Int
-                           }
+import Data.Array.IArray
+import Data.Text.Read
+import qualified Data.List as L
+import qualified Data.Text as T
 
 main = do
-    input <- readFile "day-7/input.txt"
-    print . solve $ prep input
-
-prep s = case dropWhile (==',') s of
-             "" -> []
-             s' -> (read w::Int) : prep s''
-                   where (w, s'') = break (==',') s'
-
-solve tape =
-    let allSettings = generateSettings
-    in  maximum $ map (applyInput tape) allSettings
+    rawInput <- readFile "input.txt"
+    let input   = map (\i -> read (T.unpack i) :: Int) $ T.splitOn (T.pack ",") $ T.pack rawInput
+        asArray :: Array Int Int
+        asArray = listArray (0,length input - 1) input
+        allSettings = generateSettings
+     in print $ maximum $ map (head . runWithSettings asArray) allSettings
 
 generateSettings =
-    filter ((==5) . length . List.nub) [[x, y, z, w, v] | x <- [0..4], y <- [0..4], z <- [0..4], w <- [0..4], v <- [0..4]]
+    let r = [0..4]
+     in filter ((==5) . length . L.nub) [[a,b,c,d,e] | a <- r, b <- r, c <- r, d <- r, e <- r]
 
-applyInput tape settings = foldr (runComps tape) 0 settings
+runWithSettings prgm settings =
+    let runWithSettings [] output = output
+        runWithSettings (s:ss) output =
+            runWithSettings ss $ run prgm 0 [] (s : output)
+     in runWithSettings settings [0]
 
-runComps tape setting input =
-    let state = CompState { position=0
-                          , tape=tape
-                          , input=[setting, input]
-                          , output=[]
-                          , halted=False
-                          , op=0
-                          , vals=(0, 0, 0)
-                          , modes=(0, 0, 0)
-                          , valP=0 }
+run prgm pc output input
+    | opcode == 99 = output
+    | opcode == 3  = run (prgm // [(arg1, head input)]) (pc + 2) output (tail input)
+    | opcode == 4  = run prgm (pc + 2) ((prgm ! arg1) : output) input
+    | opcode == 5  = run prgm (if arg1 /= 0 then arg2 else pc + 3) output input
+    | opcode == 6  = run prgm (if arg1 == 0 then arg2 else pc + 3) output input
+    | otherwise    = run (prgm // [(arg3, op opcode arg1 arg2)]) (pc + 4) output input
+    where opcodeModes = prgm ! pc
+          opcode      = opcodeModes `mod` 100
+          arg1        = getArg opcode prgm (opcodeModes `quot` 100 `mod` 10) (pc + 1)
+          arg2        = getArg opcode prgm (opcodeModes `quot` 1000 `mod` 10) (pc + 2)
+          arg3        = prgm ! (pc + 3)
 
-    in  runCycle state
+op 1 = (+)
+op 2 = (*)
+op 7 = (\arg1 arg2 -> if arg1 < arg2 then 1 else 0)
+op 8 = (\arg1 arg2 -> if arg1 == arg2 then 1 else 0)
 
-runCycle state =
-    let cycled = cycleState state
-    in  if halted cycled
-            then head $ output cycled
-            else runCycle cycled
-
-runNCycles 0 state = head $ output state
-runNCycles n state =
-    let cycled = cycleState state
-    in  if halted cycled
-            then head $ output cycled
-            else runNCycles (n - 1) cycled
-
-cycleState = execute . decode . fetch
-
-fetch state =
-    let opcode = tape state !! position state
-    in  state { op=mod opcode 100
-              , modes=( mod (div opcode 100) 10
-                      , mod (div opcode 1000) 10
-                      , mod (div opcode 10000) 10
-                      )
-              }
-
-decode state = state { vals= getVals state, valP=getValP state }
-
-getVals state =
-    let (mode1, mode2, _) = modes state
-    in  ( getVal 1 mode1 state
-        , getVal 2 mode2 state
-        , getVal 3     1 state
-        )
-
-getVal pos mode state
-    | op state == haltOp = 0
-    | pos > 1 && (op state == inOp || op state == outOp) = 0
-    | mode == 1 || op state == inOp = tape state !! (position state + pos)
-    | otherwise = tape state !! (tape state !! (position state + pos))
-
-getValP state
-    | op state == inOp || op state == outOp = 2
-    | op state == haltOp = 1
-    | op state == jiftOp || op state == jiffOp = 3
-    | otherwise = 4
-
-execute state =
-    let (val1, val2, val3) = vals state
-    in  (getFunc state) val1 val2 val3
-
-getFunc state =
-    case op state of
-         1  -> calcFunc (+) state
-         2  -> calcFunc (*) state
-         3  -> getInput state
-         4  -> writeOutput state
-         5  -> jumpFunc (/=0) state
-         6  -> jumpFunc (==0) state
-         7  -> calcFunc (\x y -> if x < y then 1 else 0) state
-         8  -> calcFunc (\x y -> if x == y then 1 else 0) state
-         99 -> haltFunc state
-         _ -> (\x y z -> state)
-
-calcFunc op state val1 val2 val3 =
-    let valE = op val1 val2
-    in  writeBack state val3 valE
-
-writeBack state loc val =
-    let (tapehead, tapetail) = splitAt loc $ tape state
-        newTape = tapehead ++ val : tail tapetail
-        newPos = (position state) + (valP state)
-    in  state { tape=newTape, position=newPos }
-
-getInput state val1 val2 val3 =
-    let (i:newInput) = input state
-        newState = state { input=newInput }
-    in  writeBack newState val1 i
-
-writeOutput state val1 val2 val3 =
-    let newOutput = val1 : output state
-        newPos = (position state) + (valP state)
-    in  state { output=newOutput, position=newPos }
-
-jumpFunc op state val1 val2 val3 =
-    let (val1, val2, _) = vals state
-        newPos = if op val1 then val2 else (position state) + (valP state)
-    in  state { position=newPos }
-
-haltFunc state val1 val2 val3 =
-    state { position=(position state) + (valP state)
-          , halted=True
-          }
+getArg 3 prgm mode pos = prgm ! pos
+getArg 4 prgm mode pos = prgm ! pos
+getArg opcode prgm mode pos = if mode == 1 then prgm ! pos else prgm ! (prgm ! pos)
